2729165a436ca7326c33030cd7481f86da49a1f4
Dhaanish K <freinds528@gmail.com>
2021-03-20 11:13:41 -0400

    the website is beginning
    Co-authored-by: StarNovain <StarNovain@users.noreply.github.com>
    Co-authored-by: max123480 <max123480@users.noreply.github.com>
    Co-authored-by: Crown0fThornes <Crown0fThornes@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
 from flask import Flask
+from flask import render_template
+from datetime import datetime
+import re
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template(
+        "home.html",
+    )
+
+@app.route("/hello/<name>")
+def meGongaga(name = None):
+    return render_template(
+        "test.html",
+        name=name,
+        date=datetime.now()
+    )
+
+@app.route("/survey/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
